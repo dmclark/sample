41f80c370796b55f7062c572556c166b646a4f10
David Clark <dmclark@gmail.com>
2010-08-25 19:09:27 -0400

    forgot root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Sample::Application.routes.draw do
   match '/contact',     :to => "pages#contact"
   match '/about',   :to => "pages#about"
   match '/help',     :to => "pages#help"
-  
+  root :to => 'pages#home'
    
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
