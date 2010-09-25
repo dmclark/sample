8c69f08a71722762e935bc2272326d786a3b9af7
David Clark <dmclark@gmail.com>
2010-09-25 15:13:44 -0400

    fixes for pw encrption

--- help/ruby/1.9.1/gems/rspec-expectations-2.0.0.beta.18/lib/rspec/expectations/version.rb
+++
@@ -1,7 +0,0 @@
-module RSpec # :nodoc:
-  module Expectations # :nodoc:
-    module Version # :nodoc:
-      STRING = File.readlines(File.expand_path('../../../../VERSION', __FILE__)).first
-    end
-  end
-end
--- help/ruby/1.9.1/gems/rspec-rails-2.0.0.beta.18/lib/rspec/rails/matchers/be_a_new.rb
+++
@@ -1,5 +0,0 @@
-RSpec::Matchers.define :be_a_new do |model_klass|
-  match do |actual|
-    model_klass === actual && actual.new_record?
-  end
-end
