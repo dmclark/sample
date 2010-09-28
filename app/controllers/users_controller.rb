ca4e1e3d68f5994bc4548a13b8b83d078333bffa
David Clark <dmclark@gmail.com>
2010-09-27 22:16:13 -0400

    edit user and more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :updata]
   
   def show
     @user = User.find(params[:id])
@@ -27,6 +28,23 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def authenticate
+    deny_access unless signed_in? 
   end
   
 
