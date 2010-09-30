3617501c867950411bc6a008b01b0851c1d01d09
David Clark <dmclark@gmail.com>
2010-09-29 21:56:05 -0400

    partial admin implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 # == Schema Information
-# Schema version: 20100827013149
+# Schema version: 20100930012002
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
